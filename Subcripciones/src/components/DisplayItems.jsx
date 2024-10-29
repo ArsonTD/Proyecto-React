118ce7710fb1ef1c54bb580bcadf1cb5ce615845
Anderson <arsontd28@gmail.com>
2024-10-29 15:54:47 -0400

    Añadir archivos iniciales

---
+++ Subcripciones/src/components/DisplayItems.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+import SingleItem from "./SingleItem";
+
+const DisplayItems = ({ subs, eliminarItem, editItem }) => {
+    return ( 
+        <>
+            <h2>Suscripciones</h2>
+            {
+                subs.map(item => (
+                    <SingleItem 
+                    key={item.id}
+                    id={item.id} 
+                    price={item.price} 
+                    type={item.type} 
+                    eliminarItem={eliminarItem}
+                    editItem={editItem}
+                    />
+                ))
+
+            }
+        </>
+    );
+}
+
+export default DisplayItems;
